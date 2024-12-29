7108d84137e94ea7bfa39289530209a480187ff5
neezhaa <nezhabenabdelmalek@gmail.com>
2024-12-29 16:04:35 +0100

    First commit

---
+++ src/pages/Dashboard.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Navbar from '../components/Navbar/Navbar';
+
+function Dashboard() {
+  return (
+    <div className='app-container'>
+        <Navbar/>
+        
+    </div>
+  )
+}
+
+export default Dashboard
