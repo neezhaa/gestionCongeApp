14b0bf75ab502f80873bae063fc0243f5d83636f
neezhaa <nezhabenabdelmalek@gmail.com>
2025-06-15 22:00:19 +0100

    Update login

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -2,6 +2,8 @@ import axios from 'axios';
 import { useState } from 'react'
 import { Link } from 'react-router-dom';
 import { useAuth } from "../context/AuthContext";
+import { useTranslation } from 'react-i18next';
+import { GlobeAltIcon } from '@heroicons/react/24/outline';
 
 function Login() { 
     const [email, setEmail] = useState('yassine.noussair@example.com');
@@ -9,11 +11,18 @@ function Login() {
     const [loading, setLoading] = useState(false);
     const [clicked, setIsClicked] = useState(false);
     const { login } = useAuth();
+    const { t, i18n } = useTranslation();
+
 
 
     const handleClick = () => {
         setIsClicked(prev=>!prev)
     }
+
+    const changeLanguage = (lng) => {
+        i18n.changeLanguage(lng);
+        setIsClicked(false);
+    };
 
     const handleSubmit = async(e) => {
         e.preventDefault();
@@ -45,24 +54,15 @@ function Login() {
                         bg-center bg-no-repeat bg-contain  mt-[-11px]
                         h-[17px] w-[17px]">
                     </span>
-                    Page d&apos;accueil
+                    {t('login.home_page')}
                 </Link>
 
                 <div className="flex items-center">
-                    <div onClick={handleClick} className='font-normal relative'>
+                    <div onClick={handleClick} className='font-normal relative group'>
                         <p className="flex items-center text-[#1e385b] cursor-pointer">
-                            <svg className="mr-[10px] w-[20px]" width="25" height="25" viewBox="0 0 25 25" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                <g clipPath="url(#a5152)">
-                                    <path d="M12.5 24.919c-1.645 0-3.198-.315-4.658-.946a12.164 12.164 0 0 1-3.82-2.576 12.165 12.165 0 0 1-2.576-3.82A11.619 11.619 0 0 1 .5 12.919c0-1.658.315-3.214.946-4.668a12.197 12.197 0 0 1 2.576-3.81 12.166 12.166 0 0 1 3.82-2.576C9.302 1.235 10.855.92 12.5.92c1.658 0 3.214.315 4.668.946 1.453.63 2.723 1.49 3.81 2.576a12.197 12.197 0 0 1 2.576 3.81c.63 1.454.946 3.01.946 4.668a11.62 11.62 0 0 1-.946 4.658 12.166 12.166 0 0 1-2.576 3.82 12.195 12.195 0 0 1-3.81 2.576c-1.454.63-3.01.946-4.668.946Zm0-1.922a17.26 17.26 0 0 0 1.628-2.587c.44-.87.799-1.82 1.076-2.852H9.796c.293 1.064.656 2.031 1.089 2.901.432.87.97 1.716 1.615 2.538Zm-2.446-.347a13.7 13.7 0 0 1-1.305-2.37 15.497 15.497 0 0 1-.898-2.722H3.566a9.902 9.902 0 0 0 2.684 3.307 9.017 9.017 0 0 0 3.804 1.785Zm4.892 0a9.016 9.016 0 0 0 3.804-1.785 9.902 9.902 0 0 0 2.684-3.306H17.15a19.84 19.84 0 0 1-.96 2.734c-.385.885-.8 1.67-1.243 2.357ZM2.771 15.664h4.696c-.08-.47-.137-.93-.173-1.381a17.331 17.331 0 0 1 0-2.728c.036-.451.094-.911.173-1.38H2.771a10.133 10.133 0 0 0-.28 4.151c.066.463.159.909.28 1.338Zm6.59 0h6.278a15.966 15.966 0 0 0 .225-2.745 15.966 15.966 0 0 0-.226-2.745H9.362a15.977 15.977 0 0 0-.225 2.745 15.977 15.977 0 0 0 .226 2.745Zm8.172 0h4.696a10.14 10.14 0 0 0 .28-4.151 9.955 9.955 0 0 0-.28-1.339h-4.696c.08.47.137.93.173 1.381a17.32 17.32 0 0 1 0 2.728c-.036.451-.094.911-.173 1.38Zm-.384-7.385h4.285c-.675-1.328-1.564-2.43-2.666-3.306-1.102-.876-2.376-1.475-3.822-1.797.484.735.915 1.539 1.292 2.412.378.873.681 1.77.911 2.691Zm-7.353 0h5.408a16.254 16.254 0 0 0-1.107-2.918c-.444-.89-.977-1.73-1.597-2.52-.62.79-1.153 1.63-1.597 2.52-.445.89-.814 1.862-1.107 2.918Zm-6.23 0H7.85c.23-.921.533-1.818.91-2.691.378-.873.809-1.677 1.293-2.412-1.454.322-2.73.923-3.828 1.803-1.099.88-1.985 1.98-2.66 3.3Z" fill="currentColor"></path>
-                                </g>
-                                <defs>
-                                    <clipPath id="a5152">
-                                        <path fill="currentColor" transform="translate(.5 .919)" d="M0 0h24v24H0z"></path>
-                                    </clipPath>
-                                </defs>
-                            </svg>
+                            <GlobeAltIcon className="h-5 w-5 mr-2" />
                             <span className="font-normal mt-px pr-[30px] relative">
-                                Français
+                                {i18n.language === 'fr' ? 'Français' : i18n.language === 'en' ? 'English' : 'العربية'}
                                 <span className={`flex items-center justify-center 
                                     absolute right-0 top-[-0.05rem] 
                                     bg-[url('https://pocketoption.com/themes/2017-09/img/icon-drop-down.svg')] 
@@ -72,21 +72,32 @@ function Login() {
                                 </span>
                             </span>
                         </p>
-                        <div className={`bg-white rounded-[12px] 
-                                shadow-[0_10px_70px_rgba(0,0,0,0.1)] 
-                                flex flex-col justify-evenly items-start 
-                                absolute right-0 mt-[1.1rem] p-[8px_10px] 
+                        <div className={`absolute bg-white rounded-md
+                                shadow-lg right-0 mt-2 py-1
                                 transition-transform transition-visibility duration-500 
                                 z-[999] ${clicked? "opacity-100 scale-100  pointer-events-auto visible"
                                     :
                                  "opacity-0 invisible pointer-events-none scale-0"}`}>
-                            <Link className="flex items-center rounded-[10px] text-[#1e385b] text-[14px] py-[0.313rem] px-[0.625rem] no-underline transition-all duration-500 w-[70px] hover:bg-[#f0f5f9]" href="/fr/login/">
+                            <button 
+                                onClick={() => changeLanguage('fr')}
+                                className={`block w-full text-left px-4 py-2 text-sm ${i18n.language === 'fr' ? 'bg-blue-50' : 'text-gray-700 hover:bg-gray-50'}`}
+                            >
+                                Français
+                            </button>
+
+                            <button 
+                                onClick={() => changeLanguage('en')}
+                                className={`block w-full text-left px-4 py-2 text-sm ${i18n.language === 'en' ? 'bg-blue-50' : 'text-gray-700 hover:bg-gray-50'}`}
+                            >
                                 English
-                            </Link>
+                            </button>
                         
-                            <Link className="flex items-center rounded-[10px] text-[#1e385b] text-[14px] py-[0.313rem] px-[0.625rem] no-underline transition-all duration-500 w-[70px] hover:bg-[#f0f5f9]" href="/ar/login/">
+                            <button 
+                                onClick={() => changeLanguage('ar')}
+                                className={`block w-full text-left px-4 py-2 text-sm ${i18n.language === 'ar' ? 'bg-blue-50' : 'text-gray-700 hover:bg-gray-50'}`}
+                            >
                                 العربية 
-                            </Link>
+                            </button>
                         </div>
                     </div>
 
@@ -94,12 +105,12 @@ function Login() {
             </div>
         </header>
         <div className="bg-white flex flex-col items-center justify-center max-w-[600px] rounded-[10px] pt-[47px] pb-[20px] w-full h-auto mt-[45px] mb-[20px]">
-            <h1 className='text-[32px] font-semibold tracking-[1px] leading-[1.1] m-0'>Connexion</h1>
+            <h1 className='text-[32px] font-semibold tracking-[1px] leading-[1.1] m-0'>{t('login.title')}</h1>
             <div className='text-[#535c6d] py-[12px] px-[40px] text-center'>
                     <p className="inline-block mb-[27px] whitespace-nowrap text-[#535c6d] text-center">
-                        Pas encore inscrit ?
+                        {t('login.no_account')}
                         <Link className='ml-[20px] link-b font-normal text-[#1e385b] pb-1 no-underline border-b border-[#0099fa] transition-all duration-500' to="">
-                            S&apos;inscrire
+                            {t('login.signup')}
                         </Link>
                     </p>
 
@@ -108,7 +119,7 @@ function Login() {
                         <input 
                             type="text" 
                             value={email} 
-                            placeholder='Email *' 
+                            placeholder={`${t('login.email')} *`} 
                             onChange={ e => setEmail(e.target.value)}
                             className='bg-transparent border-0 border-b border-gray-400 
                                 shadow-none text-[#758b9d] 
@@ -119,7 +130,7 @@ function Login() {
                         <input 
                             type="password" 
                             value={password} 
-                            placeholder='Mot de passe *' 
+                            placeholder={`${t('login.password')} *`} 
                             onChange={ e => setPassword(e.target.value)}
                             className='bg-transparent border-0 border-b border-gray-400 
                                 shadow-none text-[#758b9d] 
@@ -131,7 +142,7 @@ function Login() {
                             <div className="block my-[10px] relative">
                                 <p className="text-[#758b9d] text-[14px]">
                                     <input id='remember' className='absolute h-[15px] left-0 top-[3px] mt-[1px] width-[15px]' type="checkbox"/>
-                                    <label htmlFor='remember' className='pl-[23px]'>Se souvenir de moi</label>
+                                    <label htmlFor='remember' className='pl-[23px]'>{t('login.remember')}</label>
                                 </p>
                             </div>
                             <div>
@@ -141,7 +152,7 @@ function Login() {
                                     bg-center bg-no-repeat bg-contain 
                                     h-[16px] w-[16px]">
                                 </span>
-                                Mot de passe oublié ?</Link>
+                                {t('login.forgot_password')}</Link>
                             </div>
                         </div>
                     </div>
@@ -157,7 +168,7 @@ function Login() {
                                     hover:brightness-110" 
                         type="submit">
                         
-                        Se connecter
+                        {t('login.submit')}
 
                         {loading && (
                             <div className="flex items-center ml-2">
@@ -174,7 +185,7 @@ function Login() {
             </div>
         </div>
         <div className="flex items-center justify-center text-[#758b9d] text-[14px] bg-[#f0f5f9] pb-[40px] text-center">
-            Copyright ©2025 CongeEase           
+            {t('login.copyright')}                    
         </div>
     </div>
   )
